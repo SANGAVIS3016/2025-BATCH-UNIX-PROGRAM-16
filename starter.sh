32df56d4ffc56774118529501ab1cf7ff653f775
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-09 14:34:55 +0530

    Create starter.sh

---
+++ starter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# UNIX PROGRAM
+# Add a group and assign users to the group.
+
+# Write your commands below.
+
+# Step 1: Create the group
+
+
+# Step 2: Assign the users to the group
