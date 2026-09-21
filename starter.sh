b8c28203001a25953ea32ce5572999d8a9d8b120
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-21 11:05:33 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-GROUP_NAME="developers"
-USER1="student1"
-USER2="student2"
-
-# Create the group
-groupadd "$GROUP_NAME"
-
-# Add both users to the group
-usermod -aG "$GROUP_NAME" "$USER1"
-usermod -aG "$GROUP_NAME" "$USER2"
-
-echo "User and group management operations completed."
